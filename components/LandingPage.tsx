46fd4c6c990fdee18cf030a9a9e2864f5c815807
Soumadip Biswas <soumadipbiswas8@gmail.com>
2026-03-20 01:11:37 +0530

    Restore landing page motion

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence, useReducedMotion } from 'framer-motion';
 import {
